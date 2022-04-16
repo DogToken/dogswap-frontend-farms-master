ff56971d2d8e192aad0e8c992572419e724da654
Dog <80226775+DogToken@users.noreply.github.com>
2022-04-16 11:52:57 +0200

    update

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'DogToken'
           }}
           options={{
             height: '300',
